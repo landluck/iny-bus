41b9c09b85b6e397a85d6f914a96b426d1c6cd5b
landluck <245354431@qq.com>
2019-08-04 16:36:57 +0800

    build(release-docs.sh):

--- release-docs.sh
+++ release-docs.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env sh
-rm -rf docs/dist
-
-npx cross-env NODE_ENV=production webpack --config build/webpack.site.js
-
-superman-cdn cdn /vant ./docs/dist/*.js
-
-rm -rf docs/dist/*.js
-
-gh-pages -d docs/dist --add
-
-#!/usr/bin/env sh
 set -e
 read -p "begin Releasing docs - are you sure? (y/n)" -n 1 -r
 echo
